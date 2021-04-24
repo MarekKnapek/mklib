d563d66279f74cb6376ad328422a303fa6d2ffa0
Marek Knápek <knapek.mar@gmail.com>
2021-04-24 23:16:08 +0200

    ints

--- mklib/cpp/tests/tests.cpp
+++ mklib/cpp/tests/tests.cpp
@@ -1,3 +1,15 @@
+#include "../src/stdlib/ints.hpp"
+
+
 int main()
 {
+	static_assert(sizeof(mk::stdlib::uint8_t) == 1);
+	static_assert(sizeof(mk::stdlib::uint16_t) == 2);
+	static_assert(sizeof(mk::stdlib::uint32_t) == 4);
+	static_assert(sizeof(mk::stdlib::uint64_t) == 8);
+
+	static_assert(sizeof(mk::stdlib::int8_t) == 1);
+	static_assert(sizeof(mk::stdlib::int16_t) == 2);
+	static_assert(sizeof(mk::stdlib::int32_t) == 4);
+	static_assert(sizeof(mk::stdlib::int64_t) == 8);
 }
