bcb1d8e0bc5d720c88d5811c11df6174075b202d
Marek Knápek <knapek.mar@gmail.com>
2021-05-27 18:44:34 +0200

    uintptr_t

--- mklib/cpp/src/stdlib/ints.hpp
+++ mklib/cpp/src/stdlib/ints.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 
+#include "architecture.hpp"
+
+
 namespace mk
 {
 	namespace stdlib
@@ -16,6 +19,21 @@ namespace mk
 		typedef signed short int int16_t;
 		typedef signed int int32_t;
 		typedef signed long long int int64_t;
+
+
+		#if MK_STDLIB_ARCHITECTURE == MK_STDLIB_ARCHITECTURE_I386
+
+
+		typedef unsigned int uintptr_t;
+
+
+		#elif MK_STDLIB_ARCHITECTURE == MK_STDLIB_ARCHITECTURE_AMD64
+
+
+		typedef unsigned __int64 uintptr_t;
+
+
+		#endif
 
 
 	}
