754b49a82201a2ed48970b688d424020d85ca6c5
Marek Knápek <knapek.mar@gmail.com>
2021-05-29 03:45:59 +0200

    asan

--- mklib/cpp/src/runtime/asan.cpp
+++ mklib/cpp/src/runtime/asan.cpp
@@ -1,8 +1,10 @@
 #include "asan.hpp"
 
 
+#if defined(__SANITIZE_ADDRESS__)
 extern "C" void __asan_poison_memory_region(void const*, mk::stdlib::size_t);
 extern "C" void __asan_unpoison_memory_region(void const*, mk::stdlib::size_t);
+#endif
 
 
 void mk::runtime::asan_poison_memory_region(void const* const& addr, mk::stdlib::size_t const& size) noexcept
